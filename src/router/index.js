d84897afe04f04a58d71a40f880a42957ed5a47f
EXPLOO0 <1534016557@QQ.COM>
2023-08-13 16:54:23 +0800

    全局路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import Home from '@/views/layout/home'
 import Cart from '@/views/layout/cart'
 import Category from '@/views/layout/category'
 import User from '@/views/layout/user'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
@@ -38,6 +39,35 @@ const router = new VueRouter({
     { path: '/search', component: Search },
     { path: '/searchlist', component: Searchlist }
   ]
+})
+
+// 全局前置守卫
+router.beforeEach((to, from, next) => {
+  // to:到哪里去，到哪去的完整路由信息对象（路径，参数）
+  // from:从哪里来，从哪来的完整路由信息对象（路径，参数）
+  // next():是否放行
+  // (1)next()直接放行，放行到to要去的路径
+  // (2)next(路径)进行拦截，拦截到next里面配置的路径
+
+  // 定义一个数组，专门用于存放所有需要权限访问的页面
+  const authUrls = ['/pay', 'myorder']
+  // console.log(to, from, next)
+  // 看to.path是否在authUr1s中出现过
+  if (!authUrls.includes(to.path)) {
+    // 非权限页面直接放行
+    next()
+  }
+  // 是权限页面，需要验证token
+  // const token = store.state.user.userInfo.token
+  const token = store.getters.token
+  console.log(token)
+  if (!token) {
+    // 存在，放行
+    next()
+  } else {
+    // 不存在，拦截到登录
+    next('/login')
+  }
 })
 
 export default router
--- src/store/index.js
+++ src/store/index.js
@@ -9,6 +9,9 @@ export default new Vuex.Store({
 
   },
   getters: {
+    token (state) {
+      return state.user.userInfo.token
+    }
   },
   mutations: {
   },
